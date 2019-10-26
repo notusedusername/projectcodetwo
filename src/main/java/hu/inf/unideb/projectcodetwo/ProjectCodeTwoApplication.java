bf1e7ffc0a02b7793f04c1dd86cdc2172b33157e
notusedusername <xnorbikax@gmail.com>
2019-10-26 15:27:53 +0200

    Spring Boot hozzaadas

---
+++ src/main/java/hu/inf/unideb/projectcodetwo/ProjectCodeTwoApplication.java
@@ -0,0 +1,13 @@
+package hu.inf.unideb.projectcodetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCodeTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCodeTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/inf/unideb/projectcodetwo/ProjectCodeTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.inf.unideb.projectcodetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCodeTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
