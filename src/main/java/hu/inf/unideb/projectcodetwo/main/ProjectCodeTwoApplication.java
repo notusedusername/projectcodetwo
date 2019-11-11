2bbfc9202bbd5fb7e1d1a42dd9c708173de2da89
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2019-11-11 21:38:44 +0100

    hybernateconfig elksézitése

--- src/main/java/hu/inf/unideb/projectcodetwo/main/ProjectCodeTwoApplication.java
+++ src/main/java/hu/inf/unideb/projectcodetwo/main/ProjectCodeTwoApplication.java
@@ -3,11 +3,13 @@ package hu.inf.unideb.projectcodetwo.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class ProjectCodeTwoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectCodeTwoApplication.class, args);
+		run(ProjectCodeTwoApplication.class, args);
 	}
 
 }
--- src/main/java/hu/inf/unideb/projectcodetwo/model/Book.java
+++ src/main/java/hu/inf/unideb/projectcodetwo/model/Book.java
@@ -3,7 +3,6 @@ package hu.inf.unideb.projectcodetwo.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 public class Book {
