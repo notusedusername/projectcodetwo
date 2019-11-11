317ca8950fea7bf39c99b9f30bfb1d80c59f1618
SzAkos98 <asdlol0001@gmail.com>
2019-11-11 21:11:54 +0100

    Coding - Person.java

--- src/main/java/hu/inf/unideb/projectcodetwo/model/Person.java
+++ src/main/java/hu/inf/unideb/projectcodetwo/model/Person.java
@@ -1,4 +1,77 @@
 package hu.inf.unideb.projectcodetwo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
 public class Person {
+
+    @Id
+    @Column
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private Date birthDate;
+
+    @Column
+    private String adress;
+
+    public Person(Long id, String firstName, String lastName, Date birthDate, String adress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.adress = adress;
+    }
+
+    public Person() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
 }
