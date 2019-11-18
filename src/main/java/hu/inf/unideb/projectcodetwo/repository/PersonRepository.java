4898fba49be1aa0ec22957646e984327b7c6d9ef
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2019-11-18 19:29:08 +0100

    PersonRepository.java létrehozás.

--- src/main/java/hu/inf/unideb/projectcodetwo/repository/PersonRepository.java
+++ src/main/java/hu/inf/unideb/projectcodetwo/repository/PersonRepository.java
@@ -2,7 +2,12 @@ package hu.inf.unideb.projectcodetwo.repository;
 
 import hu.inf.unideb.projectcodetwo.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface PersonRepository extends JpaRepository<Person,Long> {
 
 }
