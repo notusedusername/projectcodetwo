049108161085ef3369368b7a7258d446129925e4
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2019-12-01 17:38:31 +0100

    Loan COntroler @ beilesztése

--- src/main/java/hu/inf/unideb/projectcodetwo/controller/LoanController.java
+++ src/main/java/hu/inf/unideb/projectcodetwo/controller/LoanController.java
@@ -1,4 +1,11 @@
 package hu.inf.unideb.projectcodetwo.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/codetwo", produces="application/json")
 public class LoanController {
+
 }
