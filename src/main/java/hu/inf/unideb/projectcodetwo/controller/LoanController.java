81eee2627e8871249add2a00ce0f6d788f3a6cff
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2019-12-01 17:47:38 +0100

    Loan COntroler PostMapping elksézitáése typora várva.

--- src/main/java/hu/inf/unideb/projectcodetwo/controller/LoanController.java
+++ src/main/java/hu/inf/unideb/projectcodetwo/controller/LoanController.java
@@ -1,11 +1,27 @@
 package hu.inf.unideb.projectcodetwo.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hu.inf.unideb.projectcodetwo.dto.LoanDTO;
+import hu.inf.unideb.projectcodetwo.dto.PersonDTO;
+import hu.inf.unideb.projectcodetwo.dto.ResponseDTO;
+import hu.inf.unideb.projectcodetwo.model.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/codetwo", produces="application/json")
 public class LoanController {
+
+    @PostMapping(value = "/member", consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDTO addNewPerson(@RequestBody LoanDTO resource) {
+        return personService.addPerson(modelMapper.map(resource, Person.class));
+    }
+
+    @PostMapping(value = "/member", consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDTO addNewPerson(@RequestBody LoanDTO resource) {
+        return personService.addPerson(modelMapper.map(resource, Person.class));
+    }
 
 }
